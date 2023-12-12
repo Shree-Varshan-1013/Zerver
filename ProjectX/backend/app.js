2d1dbd7d0481e25ad922bb7aa4a047d6413baad4
Shree Varshan G <shreevarshang2003@gmail.com>
2023-12-12 18:42:25 +0530

    final app changes

--- ProjectX/backend/app.js
+++ ProjectX/backend/app.js
@@ -1,8 +1,7 @@
-
-
 const http = require('http');
 const { Server } = require('socket.io');
 const dbConnect = require('./config/dbConfig');
+const checkDatabaseExistence = require('./config/checkDatabase');
 
 const server = http.createServer();
 const io = new Server(server, {
@@ -31,10 +30,10 @@ const fetchDataAndEmitArray = async (dbName, collectionName, eventName) => {
     const db = dbInstance.db(dbName);
     const logsCollection = db.collection(collectionName);
     const logDataValue = await logsCollection.find().toArray();
-    // console.log(`Got data from MongoDB (${dbName}):`, logDataValue);
+    console.log(Got data from MongoDB (${dbName}):, logDataValue);
     io.emit(eventName, { data: logDataValue });
   } catch (error) {
-    console.error(`Error fetching data from MongoDB (${dbName}):`, error);
+    console.error(Error fetching data from MongoDB (${dbName}):, error);
   }
 };
 
@@ -43,14 +42,13 @@ const fetchDataAndEmit = async (dbName, collectionName, eventName) => {
     const db = dbInstance.db(dbName);
     const logsCollection = db.collection(collectionName);
     const logDataValue = await logsCollection.findOne();
-    // console.log(`Got data from MongoDB (${dbName}):`, logDataValue);
+    console.log(Got data from MongoDB (${dbName}):, logDataValue);
     io.emit(eventName, { data: logDataValue });
   } catch (error) {
-    console.error(`Error fetching data from MongoDB (${dbName}):`, error);
+    console.error(Error fetching data from MongoDB (${dbName}):, error);
   }
 };
 
-<<<<<<< HEAD
 const fetchDataAndEmitArrayLimit = async (dbName, collectionName, eventName, limit=7) => {
   try {
     const db = dbInstance.db(dbName);
@@ -59,18 +57,15 @@ const fetchDataAndEmitArrayLimit = async (dbName, collectionName, eventName, lim
     // Use the limit method to fetch the first 'limit' documents
     const logDataValue = await logsCollection.find().limit(limit).toArray();
     
-    // console.log(`Got data from 7 (${dbName}):`, logDataValue);
+    console.log(Got data from 7 (${dbName}):, logDataValue);
     io.emit(eventName, { data: logDataValue });
   } catch (error) {
-    console.error(`Error fetching data from MongoDB (${dbName}):`, error);
+    console.error(Error fetching data from MongoDB (${dbName}):, error);
   }
 };
 
-=======
->>>>>>> 4942c754420d40e961d4f210c1ceb0d1ff556b74
 io.on('connection', async (socket) => {
-  console.log(`Client Connected: ${socket.id}`);
-  
+  console.log(Client Connected: ${socket.id});
 
   socket.on("getDBName", (dbName) => {
     console.log("database name " + dbName);
@@ -79,29 +74,34 @@ io.on('connection', async (socket) => {
   try {
 
     await connectToDatabases();
-
+   //DB FETCHES
     await fetchDataAndEmitArray("server1_clf", "basic_data", "logTableDashboard");
-
     // await fetchDataAndEmit("server2_db", "cpu_usage", "secondTable");
+    await fetchDataAndEmit("server1_clf", "operating_systems_info_security", "operatingSystem");
+    await fetchDataAndEmit("server1_clf", "vulnerabilities_count_security", "vCount");
+    await fetchDataAndEmit("server1_clf", "vulnerabilities", "vData");
+    await fetchDataAndEmitArrayLimit("server1_clf", "vulnerabilities_count_security", "vLimit");
+    await fetchDataAndEmitArray("server1_clf", "total_stars", "totalStars");
+    await fetchDataAndEmitArray("server1_clf", "cpu_usage", "cpuUsage");
+    await fetchDataAndEmitArray("server1_clf", "memory_usage", "memoryUsage");
+
 
   } catch (error) {
     console.error("Error during data fetching and emission:", error);
   }
 
   socket.on('disconnect', () => {
-    console.log(`Client Disconnected: ${socket.id}`);
+    console.log(Client Disconnected: ${socket.id});
   });
 });
 
-<<<<<<< HEAD
 const check = async () => {
   const list = await checkDatabaseExistence("mongodb+srv://test:test@log1cluster.c12lwe7.mongodb.net/?retryWrites=true&w=majority", "sasad");
   console.log(list);
 }
 check();
 
-=======
->>>>>>> 4942c754420d40e961d4f210c1ceb0d1ff556b74
+
 server.listen(3001, () => {
   console.log('Server is listening on port 3001');
 });
