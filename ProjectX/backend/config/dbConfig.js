1041b26601387da39138f87135576bac9d48606f
Shree Varshan G <shreevarshang2003@gmail.com>
2023-12-12 12:51:40 +0530

    changes - backend

---
+++ ProjectX/backend/config/dbConfig.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+const mongoURI = "mongodb+srv://test:test@log1cluster.c12lwe7.mongodb.net/?retryWrites=true&w=majority";
+let clientInstance = null;
+
+const connectToDatabase = async () => {
+  try {
+    if (!clientInstance) {
+      const client = new MongoClient(mongoURI);
+      await client.connect();
+      console.log("Connected to MongoDB");
+      clientInstance = client;
+    }
+    return clientInstance;
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    throw err;
+  }
+};
+
+module.exports = connectToDatabase;
