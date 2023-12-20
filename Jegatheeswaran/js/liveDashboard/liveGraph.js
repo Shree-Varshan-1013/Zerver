a70058b3942e8ae943716dfc3c30c287caf66c70
Sanjeevi-CS <sanjeevisakthivel2004@gmail.com>
2023-12-20 15:41:24 +0530

    commit

--- Jegatheeswaran/js/liveDashboard/liveGraph.js
+++ Jegatheeswaran/js/liveDashboard/liveGraph.js
@@ -1,97 +1,91 @@
-var dom = document.getElementById('chart-container');
-var myChart = echarts.init(dom, 'dark', {
-  renderer: 'canvas',
-  useDirtyRect: false
-});
+  var dom = document.getElementById('chart-container');
+  var myChart = echarts.init(dom, 'dark', {
+    renderer: 'canvas',
+    useDirtyRect: false
+  });
 
-var option;
+  setInterval(function () {
 
-let now = +new Date();
-
-// Assuming one data point per second for the past 60 seconds
-let oneSecond = 1000;
-let numberOfDataPoints = 3600;
-let randomValues = Array.from({ length: numberOfDataPoints }, () =>
-  Math.round(Math.abs((Math.random() + 0.5) * 20))
-);
-
-let data = randomValues.map((value, index) => [now - (numberOfDataPoints - index - 1) * oneSecond, value]);
-
-option = {
-  tooltip: {
-    trigger: 'axis',
-    position: function (pt) {
-      return [pt[0], '10%'];
-    }
-  },
-  title: {
-    left: 'center',
-    text: 'Large Area Chart'
-  },
-  toolbox: {
-    feature: {
-      dataZoom: {
-        yAxisIndex: 'none'
-      },
-      restore: {},
-      saveAsImage: {}
-    }
-  },
-  xAxis: {
-    type: 'time',
-    boundaryGap: false
-  },
-  yAxis: {
-    type: 'value',
-    boundaryGap: [0, '100%']
-  },
-  dataZoom: [
-    {
-      type: 'inside',
-      start: 80,
-      end: 100
-    },
-    {
-      start: 80,
-      end: 100
-    }
-  ],
-  series: [
-    {
-      name: 'Fake Data',
+  var options = {
+    chart: {
       type: 'line',
-      showSymbol: false,
-      lineStyle: {  // Set the line style here
-        width: 1  // Adjust the width as needed
-      },
-      data: data
-    }
-  ]
-};
-
-myChart.setOption(option);
-
-// Update chart dynamically every second with smooth animation
-setInterval(function () {
-  let newTimestamp = +new Date();
-  let randomValue = Math.round((Math.random(1,1000)) * 20);
-
-  // Add the new random value to the array
-  randomValues.push(randomValue);
-
-  // Remove the oldest random value
-  randomValues.shift();
-
-  // Update the chart data with the updated array and enable smooth animation
-  myChart.setOption({
+      animations: {
+        enabled: true,
+        easing: 'easeInOutQuad', // Use 'easeInOutQuad' easing for a smoother effect
+        speed: 500, // Animation duration in milliseconds
+        animateGradually: {
+          enabled: true,
+          delay: 50 // Reduce the delay to 50 milliseconds between each data point's animation
+        },
+        dynamicAnimation: {
+          enabled: true,
+          speed: 350
+        }
+      }
+    },
     series: [
       {
-        data: randomValues.map((value, index) => [newTimestamp - (numberOfDataPoints - index - 1) * oneSecond, value])
+        name: 'Fake Data',
+        data: randomValues
       }
     ],
-    animation: true,
-    animationDuration: 1000
+    xaxis: {
+      type: 'datetime',
+      labels: {
+        format: 'dd/MM/yyyy HH:mm:ss'
+      }
+    },
+    yaxis: {
+      labels: {
+        formatter: function (val) {
+          return val.toFixed(2); // Format y-axis labels as needed
+        }
+      }
+    },
+    title: {
+      text: 'Large Area Chart',
+      align: 'center'
+    },
+    tooltip: {
+      x: {
+        format: 'dd/MM/yyyy HH:mm:ss'
+      }
+    }
+  };
+  
+  var chart = new ApexCharts(document.getElementById('chart-container'), options);
+  chart.render();
+  
+  // Update chart dynamically every second with smooth animation
+  setInterval(function () {
+    let newTimestamp = new Date().getTime();
+    let randomValue = Math.round((Math.random(1, 1000)) * 20);
+  
+    // Add the new random value to the array
+    randomValues.push(randomValue);
+  
+    // Remove the oldest random value
+    randomValues.shift();
+  
+    // Update the chart data with the updated array and enable smooth animation
+    chart.updateSeries([
+      {
+        data: randomValues.map((value, index) => ({
+          x: newTimestamp - (numberOfDataPoints - index - 1) * oneSecond,
+          y: value
+        }))
+      }
+    ]);
+  }, 1000);
+  
+  // Resize the chart on window resize
+  window.addEventListener('resize', function () {
+    chart.updateOptions({
+      chart: {
+        animations: {
+          enabled: true
+        }
+      }
+    });
+    chart.resize();
   });
-}, 1000);
-
-window.addEventListener('resize', myChart.resize);
